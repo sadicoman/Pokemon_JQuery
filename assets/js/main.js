8f37880b3f1e327bd5e8eb90f4c1f494964b994c
Francois Szczepkowski <francois.szcz@hotmail.fr>
2023-09-15 14:45:04 +0200

    col description

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,9 +32,12 @@
 /*************************** 
 		Exo Pokemon 
 ****************************/
+// Tableau pour stocker les lignes du tableau
+let tableRows = [];
+
 // Fonction AJAX pour récupérer les données des Pokémon depuis la PokeAPI
 $.ajax({
-    url: "https://pokeapi.co/api/v2/pokemon?limit=10",  // Limite à 10 Pokémon pour cet exemple
+    url: "https://pokeapi.co/api/v2/pokemon?limit=10",
     type: "GET",
     success: function(data) {
         // Boucle à travers chaque Pokémon
@@ -44,18 +47,44 @@ $.ajax({
                 url: pokemon.url,
                 type: "GET",
                 success: function(pokeData) {
-                    // Création d'une nouvelle ligne de tableau pour ce Pokémon
-                    let newRow = $("<tr></tr>");
-                    
-                    // Ajout de l'ID, du nom et de l'image du Pokémon à la nouvelle ligne
-                    newRow.append("<td>" + pokeData.id + "</td>");
-                    newRow.append("<td>" + pokeData.name + "</td>");
-                    newRow.append("<td><img src='" + pokeData.sprites.front_default + "' /></td>");
-                    
-                    // Ajout de la nouvelle ligne au tableau
-                    $("#pokemon-table tbody").append(newRow);
+                    // Récupérer le nom et le flavor_text en français
+                    $.ajax({
+                        url: pokeData.species.url,
+                        type: "GET",
+                        success: function(speciesData) {
+                            const frenchName = speciesData.names.find(name => name.language.name === 'fr').name;
+                            const frenchFlavorText = speciesData.flavor_text_entries.find(text => text.language.name === 'fr');
+
+                            // Création d'une nouvelle ligne de tableau pour ce Pokémon
+                            let newRow = $("<tr></tr>");
+
+                            // Ajout de l'ID, du nom, du nom en français, du flavor_text et de l'image du Pokémon à la nouvelle ligne
+                            newRow.append("<td>" + pokeData.id + "</td>");
+                            newRow.append("<td>" + pokeData.name + "</td>");
+                            newRow.append("<td>" + frenchName + "</td>");
+                            newRow.append("<td>" + (frenchFlavorText ? frenchFlavorText.flavor_text : 'N/A') + "</td>");
+                            newRow.append("<td><img src='" + pokeData.sprites.front_default + "' /></td>");
+
+                            // Ajout de la nouvelle ligne au tableau de lignes
+                            tableRows.push({id: pokeData.id, row: newRow});
+
+                            // Vérifie si toutes les lignes ont été créées
+                            if (tableRows.length === data.results.length) {
+                                // Trie les lignes par ID
+                                tableRows.sort((a, b) => a.id - b.id);
+
+                                // Ajoute les lignes triées au tableau
+                                tableRows.forEach(rowObj => {
+                                    $("#pokemon-table tbody").append(rowObj.row);
+                                });
+                            }
+                        }
+                    });
                 }
             });
         });
     }
 });
+
+
+
